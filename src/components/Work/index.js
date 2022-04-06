bead1d3b5ff417c499ccd6dfea08363f255fefac
camilo <dcserratot@correo.udistrital.edu.co>
2022-04-06 11:08:44 -0500

    adaptando falta agregar skills, correo, y experiencia o sea los proyectos que he realizado

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -3,6 +3,7 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import image1 from "../../assets/images/analitico.png";
+import image2 from "../../assets/images/first.png";
 export const Work = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
 
@@ -54,11 +55,32 @@ export const Work = () => {
               />
             </h1>
             <div>
+            <a href=" https://github.com/AnaliticoVegetales/Frontend" target = "_blank" rel="noreferrer">
               <img
                 className="igual-relacion"
                 src={image1}
                 alt="Analitico"
               ></img>
+              </a>
+              
+            </div>
+            <br/>
+            
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={["First", "  ", "Solution"]}
+                idx={15}
+              />
+            </h1>
+            <div>
+              <a href="https://dry-sierra-51774.herokuapp.com" target = "_blank" rel="noreferrer">
+              <img
+                className="relacion"
+                src={image2}
+                alt="Analitico"
+              ></img>
+              </a>
             </div>
           </div>
           
