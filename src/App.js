b120321771562635074f0dbbcba1b0e17b60ec07
camilo <dcserratot@correo.udistrital.edu.co>
2022-04-18 19:28:44 -0500

    intent

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import './App.scss'
 function App() {
   return (
     <>
-      <Routes>
+
+      <Routes baseName={process.env.PUBLIC_URL}>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
@@ -17,6 +18,7 @@ function App() {
           <Route path="/work" element={<Work />} />
         </Route>
       </Routes>
+
     </>
   )
 }
